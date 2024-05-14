68a89b6aed8a998a392cfcbb03fdbfac46f34f75
Damir Dupanovic <damir.dupanovic@iwgplc.com>
2024-05-14 22:11:18 +0200

    Week 2 commit - add use state

--- Week 2/vite-project/src/App.tsx
+++ Week 2/vite-project/src/App.tsx
@@ -3,14 +3,26 @@ import Header2 from './components/header2'
 import About from './components/about'
 import Footer from './components/footer'
 import ProjectList from './components/project-list'
+import {useState} from "react" //import useState to use it, this is a hook
+import Input from './components/input'
 
-
+//you can only use hook in functional components
 function App() {
-  
+  //whenever the value "name" is updated it will rerender the page
+  const [name, setName] = useState<string>();
+
+
   return (
     <>
       <Header2 name='Damir D.' tagline='Solutions that speak for themselves.'></Header2>
       <About bio='Software developer. Likes to code.'></About>
+      
+      <Input onUpdate={(value)=> setName(value)}></Input>
+      {/*
+      you can send method like this to use previousli saved value...
+      <Input onUpdate={(value)=> setName((previous) => previous + value)}></Input> 
+      */}
+      {name}
       <ProjectList></ProjectList>
       <Footer phoneNumber='+38761123123' email='test@test.com'></Footer>
     </>
--- Week 2/vite-project/src/components/project-list.tsx
+++ Week 2/vite-project/src/components/project-list.tsx
@@ -9,7 +9,7 @@ const ProjectList = () => {
                 {
                     projects.map((proj, index) => {
                         return (
-                            //you need to provide the key here, not inside the component
+                            //IMPORTANT: you need to provide the key here, not inside the component
                             <ProjectListItem key={proj.id} index={index} project={proj}></ProjectListItem>
                     )
                 })}
