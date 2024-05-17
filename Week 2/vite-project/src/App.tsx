a5d4ae4a72012d76302c49f404556fadb1c2dd64
Damir Dupanovic <damir.dupanovic@iwgplc.com>
2024-05-17 18:18:48 +0200

    Week 2 commit - Enable calling an api

--- Week 2/vite-project/src/App.tsx
+++ Week 2/vite-project/src/App.tsx
@@ -3,21 +3,31 @@ import Header2 from './components/header2'
 import About from './components/about'
 import Footer from './components/footer'
 import ProjectList from './components/project-list'
-import {useState} from "react" //import useState to use it, this is a hook
+import {useState, useEffect} from "react" //import useState to use it, this is a hook
 import Input from './components/input'
 
 //you can only use hook in functional components
 function App() {
   //whenever the value "name" is updated it will rerender the page
-  const [name, setName] = useState<string>();
+  //const [name, setName] = useState<string>();
 
+  //takes two parameters, a method and a array
+  //method will fire whenever there is a change on a parameter in the array
+  //if the array is empty it will fire only once
+  useEffect(() =>{
+    console.log("test use effect once"); //rendered onece at begining
+  }, [])
+
+  // useEffect(() =>{
+  //   console.log(`test use effect once ${name}`); //rendered onece at beginging, rendered whenever there is chagne to name
+  // }, [name])
 
   return (
     <>
       <Header2 name='Damir D.' tagline='Solutions that speak for themselves.'></Header2>
       <About bio='Software developer. Likes to code.'></About>
       
-      <Input onUpdate={(value)=> setName(value)}></Input>
+      {/* <Input onUpdate={(value)=> setName(value)}></Input> */}
       {/*
       you can send method like this to use previousli saved value...
       <Input onUpdate={(value)=> setName((previous) => previous + value)}></Input> 
--- Week 2/vite-project/src/components/project-list.tsx
+++ Week 2/vite-project/src/components/project-list.tsx
@@ -1,9 +1,35 @@
-import projects from "../assets/projects.json"
+import Input from "./input";
 import ProjectListItem from "./project-list-item";
+import { Project } from '../models/project.model';
+import {useState, useEffect} from 'react';
+
 
 const ProjectList = () => {
-     
+
+    const [filter, setFilter] = useState<string>("");
+    const [projects, setProjects] = useState<Project[]>([]);
+
+    // useEffect(() =>{
+    //     fetch("http://localhost:3000/projects")
+    //         .then((res) => res.json())
+    //         .then((data) => setProjects(data))
+    // }, []);
+
+    //this does not work
+    // useEffect(() => {
+    //     projects.filter((p) => 
+    //         p.name.toLowerCase().includes(filter.toLowerCase())
+    //     );
+    // }, [filter])
+
+    useEffect(() =>{
+        fetch(`http://localhost:3000/projects?name_like=${filter}`)
+            .then((res) => res.json())
+            .then((data) => setProjects(data))
+    }, [filter]);
+
     return <div>
+        <Input placeholder='Search...' onUpdate={(value)=>setFilter(value)}></Input>
         {
             <div>
                 {
