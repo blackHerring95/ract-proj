6e1989cbdbbe2910c65f2d88f9847c302756c6c9
Damir Dupanovic <damir.dupanovic@iwgplc.com>
2024-05-19 20:25:45 +0200

    Week 2 commit - Conditional rendering

--- Week 2/vite-project/src/components/project-list.tsx
+++ Week 2/vite-project/src/components/project-list.tsx
@@ -8,7 +8,7 @@ const ProjectList = () => {
 
     const [filter, setFilter] = useState<string>("");
     const [projects, setProjects] = useState<Project[]>([]);
-
+    const [loading, setLoading] = useState<boolean>(false);
     // useEffect(() =>{
     //     fetch("http://localhost:3000/projects")
     //         .then((res) => res.json())
@@ -22,24 +22,29 @@ const ProjectList = () => {
     //     );
     // }, [filter])
 
+    //if you send multiple requests at the same time previous ones will not be cancled
+    //this can be handled by a library or cancelation tokens
+    //it can be done with falg, and use conditional loading 
     useEffect(() =>{
+        setLoading(true);
         fetch(`http://localhost:3000/projects?name_like=${filter}`)
             .then((res) => res.json())
             .then((data) => setProjects(data))
-    }, [filter]);
+            .finally(() => setLoading(false))
+    }, [filter]); 
 
     return <div>
         <Input placeholder='Search...' onUpdate={(value)=>setFilter(value)}></Input>
         {
-            <div>
-                {
-                    projects.map((proj, index) => {
-                        return (
-                            //IMPORTANT: you need to provide the key here, not inside the component
-                            <ProjectListItem key={proj.id} index={index} project={proj}></ProjectListItem>
-                    )
-                })}
-            </div>
+            loading ? (
+                <div>Loading...</div>
+            ) : (
+                <>
+                    {projects.map((proj, index) => (
+                        <ProjectListItem key={proj.id} index={index} project={proj}></ProjectListItem>
+                    ))}
+                </>
+            )
         }
     </div>
 };
