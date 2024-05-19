42bfef9ba8de8faf3098407098a4e8060544aa22
Damir Dupanovic <damir.dupanovic@iwgplc.com>
2024-05-19 21:34:09 +0200

    Week 2 commit - Add routing

--- Week 2/vite-project/src/main.tsx
+++ Week 2/vite-project/src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element : <div>Hello...</div>
+  },
+  {
+    path:"/projects",
+    element : <App></App>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* add routing */}
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
